47e1e27e6007e62f3727f572289d08c9d2c63866
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-02-26 13:20:29 -0300

    feat: :sparkles: Exibindo os contatos dinamicamente com @Input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,9 @@ import { SeparadorComponent } from './componentes/separador/separador.component'
     CommonModule,
     ContainerComponent,
     CabecalhoComponent,
-    SeparadorComponent
-],
+    SeparadorComponent,
+    ContatoComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
