4dc32700facc5ef20a111ab08e4c90823f6e2d83
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-02-27 18:59:07 -0300

    feat: :sparkles: usando o localStorage para salvar lista de contatos

--- src/app/services/contato.service.ts
+++ src/app/services/contato.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@angular/core';
 
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class ContatoService {
-  constructor() {}
+  private contatos: Contato[] = [
+    { id: 1, nome: 'Ana', telefone: '29 278869420' },
+    { id: 2, nome: 'Ágata', telefone: '38 128451235' },
+    { id: 3, nome: 'Beatriz', telefone: '25 854986459' },
+    { id: 4, nome: 'Cláudia', telefone: '31 176437098' },
+    { id: 5, nome: 'Diana', telefone: '16 670764734' },
+  ];
+  constructor() {
+    const contatosLocalStorageString = localStorage.getItem('contatos');
+    const contatosLocalStorage = contatosLocalStorageString
+      ? JSON.parse(contatosLocalStorageString)
+      : null;
+
+    this.contatos = contatosLocalStorage || null;
+
+    localStorage.setItem('contatos', JSON.stringify(this.contatos));
+  }
+
+  obterContatos() {
+    return this.contatos;
+  }
 }
