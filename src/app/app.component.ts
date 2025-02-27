2c5278a6b1279bb26b0cc6b79dffa36ab67d695b
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-02-27 17:22:05 -0300

    feat: :sparkles: Configurando o arquivo de rotas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
-import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    FormularioContatoComponent,
-    ListaContatosComponent,
-  ],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
