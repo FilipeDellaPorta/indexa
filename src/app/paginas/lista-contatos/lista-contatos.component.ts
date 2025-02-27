aceab9dcb0eb8115c4cd9cf1203f7027f4ed0efb
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-02-27 17:12:41 -0300

    refactor: :recycle: refatorando código em preparação para router

---
+++ src/app/paginas/lista-contatos/lista-contatos.component.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ContainerComponent } from '../../componentes/container/container.component';
+import { CabecalhoComponent } from '../../componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from '../../componentes/separador/separador.component';
+import { ContatoComponent } from '../../componentes/contato/contato.component';
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+import agenda from '../../agenda.json';
+
+@Component({
+  selector: 'app-lista-contatos',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule,
+  ],
+  templateUrl: './lista-contatos.component.html',
+  styleUrl: './lista-contatos.component.css',
+})
+export class ListaContatosComponent {
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
+
+  filtroPorTexto: string = '';
+
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filtrarContatosPorTexto(): Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos;
+    }
+    return this.contatos.filter((contato) => {
+      return this.removerAcentos(contato.nome)
+        .toLowerCase()
+        .includes(this.removerAcentos(this.filtroPorTexto).toLowerCase());
+    });
+  }
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.filtrarContatosPorTexto().filter((contato) => {
+      return this.removerAcentos(contato.nome)
+        .toLowerCase()
+        .startsWith(this.removerAcentos(letra).toLowerCase());
+    });
+  }
+}
