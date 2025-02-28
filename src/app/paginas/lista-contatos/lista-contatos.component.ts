ee0d96abaed600d0f4d2d06dea49c838fe334836
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-02-28 17:17:53 -0300

    feat: :sparkles: criando a tela de perfil de contato

--- src/app/paginas/lista-contatos/lista-contatos.component.ts
+++ src/app/paginas/lista-contatos/lista-contatos.component.ts
@@ -8,6 +8,7 @@ import { SeparadorComponent } from '../../componentes/separador/separador.compon
 import { ContatoComponent } from '../../componentes/contato/contato.component';
 import { ContatoService } from '../../services/contato.service';
 import { Contato } from '../../componentes/contato/contato';
+import { PerfilContatoComponent } from '../perfil-contato/perfil-contato.component';
 
 @Component({
   selector: 'app-lista-contatos',
@@ -20,6 +21,7 @@ import { Contato } from '../../componentes/contato/contato';
     ContatoComponent,
     FormsModule,
     RouterLink,
+    PerfilContatoComponent,
   ],
   templateUrl: './lista-contatos.component.html',
   styleUrl: './lista-contatos.component.css',
