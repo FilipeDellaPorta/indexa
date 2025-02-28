83a724d7c49e82e6f6983ed39853e1894d936c56
FilipeDellaPorta <filipedellaporta1995@gmail.com>
2025-02-28 16:14:00 -0300

    feat: :sparkles: refatorado código para consumir contatos da api e lidado com os observables

--- src/app/services/contato.service.ts
+++ src/app/services/contato.service.ts
@@ -1,59 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Contato } from '../componentes/contato/contato';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContatoService {
-  private contatos: Contato[] = [
-    {
-      id: 1,
-      nome: 'Ana',
-      telefone: '29 278869420',
-      email: 'agenda@corporativo.com',
-    },
-    {
-      id: 2,
-      nome: 'Ágata',
-      telefone: '38 128451235',
-      email: 'agenda@corporativo.com',
-    },
-    {
-      id: 3,
-      nome: 'Beatriz',
-      telefone: '25 854986459',
-      email: 'agenda@corporativo.com',
-    },
-    {
-      id: 4,
-      nome: 'Cláudia',
-      telefone: '31 176437098',
-      email: 'agenda@corporativo.com',
-    },
-    {
-      id: 5,
-      nome: 'Diana',
-      telefone: '16 670764734',
-      email: 'agenda@corporativo.com',
-    },
-  ];
-  constructor() {
-    const contatosLocalStorageString = localStorage.getItem('contatos');
-    const contatosLocalStorage = contatosLocalStorageString
-      ? JSON.parse(contatosLocalStorageString)
-      : null;
+  private readonly API = 'http://localhost:3000/contatos';
 
-    this.contatos = contatosLocalStorage || this.contatos;
+  constructor(private http: HttpClient) {}
 
-    localStorage.setItem('contatos', JSON.stringify(this.contatos));
+  obterContatos(): Observable<Contato[]> {
+    return this.http.get<Contato[]>(this.API);
   }
 
-  obterContatos() {
-    return this.contatos;
-  }
-
-  salvarContato(contato: Contato) {
-    this.contatos.push(contato);
-    localStorage.setItem('contatos', JSON.stringify(this.contatos));
-  }
+  salvarContato(contato: Contato) {}
 }
